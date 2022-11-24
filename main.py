4baf972f724084a4aed89ceccdedcca171288a96
Fryderyk Róg <s180265@student.pg.edu.pl>
2022-11-24 12:56:19 +0100

    created methods responsible for creating init population and computing fitness

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
+import numpy as np
+from utils import Item, dataReader, arreq_in_list
 
-class dataReader:
-    @staticmethod
-    def read_data(dataset_id):
-        #weights
-        with open(f"./data/{dataset_id}_w.txt") as f:
-            weights = [int(line) for line in f]
-        #profits
-        with open(f"./data/{dataset_id}_p.txt") as f:
-            profits = [int(line) for line in f]
-        #capacity
-        with open(f"./data/{dataset_id}_c.txt") as f:
-            capacity = int(f.readline())
-        #opt. selection
-        with open(f"./data/{dataset_id}_s.txt") as f:
-            optimal_selection = [int(line) for line in f]
+class GAKnapsackSolver:
+    def __init__(self, weights, profits, capacity, optimal_selection) -> None:
+        self.weights = np.array(weights)
+        self.profits = np.array(profits)
+        self.knapsack_capacity = capacity 
+        self.optimal_selection = optimal_selection
+        self.item_list = []
 
-        return weights, profits, capacity, optimal_selection
+    def compute_fitness(self):
+        self.fitness = np.empty(len(self.population))
+        for i,specimen in enumerate(self.population):
+            specimen_fitness = np.dot(specimen, self.profits)
+            print(specimen_fitness)
+            specimen_weight = np.dot(specimen, self.weights)
+            print(specimen_weight)
+            if specimen_weight > self.knapsack_capacity:
+                specimen_fitness = 0
+            self.fitness[i] = specimen_fitness
+
+
+    def create_initial_population(self, population_size):
+        self.population = list()
+        while len(self.population) != population_size:
+            individiual_bits = np.random.randint(2, size=len(self.item_list))
+            if not arreq_in_list(individiual_bits, self.population):
+                self.population.append(individiual_bits)
+
+        
 
 if __name__ == '__main__':
     weights, profits, capacity, optimal_selection = dataReader.read_data("p01")
-    print(weights, profits, capacity, optimal_selection)
+    #print(weights, profits, capacity, optimal_selection)
 
-        
+    genetic_alghoritm = GAKnapsackSolver(weights, profits, capacity, optimal_selection)
+    genetic_alghoritm.create_initial_population(5)
+    genetic_alghoritm.fitness_calc()
+    print(genetic_alghoritm.fitness)
 
